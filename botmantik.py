49bb0cc10119b16f58eaed8bc361e0ac9f63e6a7
GülND <gnazd0829@gmail.com>
2025-02-08 22:14:42 +0300

    Create botmantik.py

---
+++ botmantik.py
@@ -0,0 +1,15 @@
+import random
+def sifre_olusturucu(sifre_uzunlugu):
+    ogeler = "+-/*!&$#?=@<>"
+    sifre = ""
+
+    for i in range(sifre_uzunlugu):
+        sifre += random.choice(ogeler)
+
+    return sifre
+def yazi_tura():
+    para = random.randint(0, 2)
+    if para == 0:
+        return "YAZI"
+    else:
+        return "TURA"
